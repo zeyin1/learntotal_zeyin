4f18e46a062a908bdd1b5effa8d31273820d7320
zeyin1 <you@example.com>
2021-11-07 13:20:40 +0800

    初始化项目

---
+++ zeyin-pro/src/main/java/com/zeyinpro/ZeyinProApplication.java
@@ -0,0 +1,13 @@
+package com.zeyinpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeyinProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeyinProApplication.class, args);
+    }
+
+}
